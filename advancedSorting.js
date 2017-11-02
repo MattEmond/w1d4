02fa7c9a33167adc85fd867b7ef17c3accc6b091
MattEmond <memond36@gmail.com>
2017-11-02 17:55:19 +0000

    More code

--- advancedSorting.js
+++ advancedSorting.js
@@ -5,4 +5,17 @@ var students = [
   { id: 4, name: "alex",     age: 30 }
 ];
 
-console.log(students)
+students.sort(function(a,b) {
+  var nameA = a.name.toUpperCase();
+  var nameB = b.name.toUpperCase();
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+
+  return 0;
+});
+
+console.log(students);
