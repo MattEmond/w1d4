3465b825030752db594804e21753e1eca2d92423
MattEmond <memond36@gmail.com>
2017-11-02 16:05:20 +0000

    forEachArray

---
+++ forEachArray.js
@@ -0,0 +1,20 @@
+// The second argument/parameter is expected to be a function
+function findWaldo(arr, found) {
+  arr.forEach(function(arr) {
+    if (arr === 'Waldo') {
+      found()
+    }
+  })
+
+  // for (var i = 0; i < arr.length; i++) {
+  //   if (arr[i] === "Waldo") {
+  //     found();   // execute callback
+  //   }
+  // }
+};
+
+function actionWhenFound() {
+  console.log("Found him!");
+}
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
